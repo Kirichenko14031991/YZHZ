33a105878d5fe5ea00e665e743694a4f578f2b08
GenaKirichenko <Bravltop008@gmail.com>
2025-01-23 15:25:55 +0200

    bru.js

--- src/components/cities/belgia/bru.js
+++ src/components/cities/belgia/bru.js
@@ -17,44 +17,45 @@ export function openBruInfo() {
 
     <h2>Проживання та житлова допомога</h2>
     <ul>
-        <li><a href="#" target="_blank">Програма міської ради – CITY OF BRUSSELS</a></li>
-        <li><a href="#" target="_blank">Пошук житла – Office of the commissioner general for refugees and stateless persons</a></li>
+        <li><a href="https://www.brussels.be/refugees" target="_blank">Програма міської ради – CITY OF BRUSSELS</a></li>
+        <li><a href="ITY OF BRUSSELS (Refugees)</a></li>
+          <li><a href="https://www.cgvs.be/en" target="_blank">Пошук житла – Office of the commissioner general for refugees and stateless persons</a></li>
     </ul>
 
     <h2>Юридична підтримка</h2>
     <ul>
-        <li><a href="#" target="_blank">Допомога з реєстрацією – CITY OF BRUSSELS (Registration)</a></li>
-        <li><a href="#" target="_blank">Подання заяви на тимчасовий захист</a></li>
+        <li><a href="https://www.brussels.be/registration" target="_blank">Допомога з реєстрацією – CITY OF BRUSSELS (Registration)</a></li>
+        <li><a href="https://www.cgvs.be/en/protection" target="_blank">Подання заяви на тимчасовий захист</a></li>
     </ul>
 
     <h2>Фінансова допомога</h2>
     <ul>
-        <li><a href="#" target="_blank">Матеріальна підтримка – The CPAS</a></li>
-        <li><a href="#" target="_blank">Служба зайнятості – Actiris</a></li>
+        <li><a href="https://www.cpas.brussels" target="_blank">Матеріальна підтримка – The CPAS</a></li>
+        <li><a href="https://www.actiris.be" target="_blank">Служба зайнятості – Actiris</a></li>
     </ul>
 
     <h2>Медична допомога</h2>
     <ul>
-        <li><a href="#" target="_blank">Екстрена допомога – Медичне обслуговування українських біженців</a></li>
-        <li><a href="#" target="_blank">Щеплення – Vivalis Brussels</a></li>
+        <li><a href="https://www.brussels.be/healthcare" target="_blank">Екстрена допомога – Медичне обслуговування українських біженців</a></li>
+        <li><a href="https://www.vivalia.be" target="_blank">Щеплення – Vivalis Brussels</a></li>
     </ul>
 
     <h2>Освіта та інтеграція</h2>
     <ul>
-        <li><a href="#" target="_blank">Ознайомлення з Брюсселем</a></li>
-        <li><a href="#" target="_blank">Доступ до місцевих шкіл</a></li>
+        <li><a href="https://www.integration.brussels" target="_blank">Ознайомлення з Брюсселем</a></li>
+        <li><a href="https://www.brussels.be/schools" target="_blank">Доступ до місцевих шкіл</a></li>
     </ul>
 
     <h2>Працевлаштування</h2>
     <ul>
-        <li><a href="#" target="_blank">Служба зайнятості – Actiris</a></li>
-        <li><a href="#" target="_blank">Робота на будівництві</a></li>
+        <li><a href="https://www.actiris.be" target="_blank">Служба зайнятості – Actiris</a></li>
+        <li><a href="https://www.buildingjobs.be" target="_blank">Робота на будівництві</a></li>
     </ul>
 
     <h2>Психологічна підтримка</h2>
     <ul>
-        <li><a href="#" target="_blank">Психологічна допомога – Mental Health Brussels</a></li>
-        <li><a href="#" target="_blank">Консультації – Карітас Бельгія</a></li>
+        <li><a href="https://www.mentalhealth.brussels" target="_blank">Психологічна допомога – Mental Health Brussels</a></li>
+        <li><a href="https://www.caritas.be" target="_blank">Консультації – Карітас Бельгія</a></li>
     </ul>
 
     <h3>Контакти:</h3>
