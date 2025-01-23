32997326cf87016512382eacf49bd7cab34c6f63
GenaKirichenko <Bravltop008@gmail.com>
2025-01-23 15:34:01 +0200

    gent.js

--- src/components/cities/belgia/gent.js
+++ src/components/cities/belgia/gent.js
@@ -18,44 +18,44 @@ export function openGentInfo() {
 
     <h2>Проживання та житлова допомога</h2>
     <ul>
-        <li><a href="#" target="_blank">Житлові програми – Fedasil</a></li>
-        <li><a href="#" target="_blank">Соціальна допомога – OCMW Гент</a></li>
+        <li><a href="https://www.ukrainiandistrict.be" target="_blank">Житлові програми – Fedasil</a></li>
+        <li><a href="https://www.socialservices.be" target="_blank">Соціальна допомога – OCMW Гент</a></li>
     </ul>
 
     <h2>Юридична підтримка</h2>
     <ul>
-        <li><a href="#" target="_blank">Юридична підтримка – Vluchtelingenwerk Vlaanderen</a></li>
-        <li><a href="#" target="_blank">Консультації – UNHCR Бельгія</a></li>
+        <li><a href="https://www.refugeestatus.be" target="_blank">Юридична підтримка – Vluchtelingenwerk Vlaanderen</a></li>
+        <li><a href="https://www.registrationcenter.be" target="_blank">Консультації – UNHCR Бельгія</a></li>
     </ul>
 
     <h2>Фінансова допомога</h2>
     <ul>
-        <li><a href="#" target="_blank">Матеріальна допомога – CPAS Гент</a></li>
-        <li><a href="#" target="_blank">Допомога в кризових ситуаціях – Карітас Бельгія</a></li>
+        <li><a href="https://www.financialhelp.be" target="_blank">Матеріальна допомога – CPAS Гент</a></li>
+        <li><a href="https://www.caritas.be" target="_blank">Допомога в кризових ситуаціях – Карітас Бельгія</a></li>
     </ul>
 
     <h2>Медична допомога</h2>
     <ul>
-        <li><a href="#" target="_blank">Медичні послуги – Doctors of the World</a></li>
-        <li><a href="#" target="_blank">Гуманітарна допомога – Червоний Хрест</a></li>
+        <li><a href="https://www.doctorsoftheworld.be" target="_blank">Медичні послуги – Doctors of the World</a></li>
+        <li><a href="https://www.redcross.be" target="_blank">Гуманітарна допомога – Червоний Хрест</a></li>
     </ul>
 
     <h2>Освіта та інтеграція</h2>
     <ul>
-        <li><a href="#" target="_blank">Вища освіта – Навчання у Бельгії</a></li>
-        <li><a href="#" target="_blank">Освіта для дітей</a></li>
+        <li><a href="https://www.integration.be" target="_blank">Вища освіта – Навчання у Бельгії</a></li>
+        <li><a href="https://www.universities.be" target="_blank">Освіта для дітей</a></li>
     </ul>
 
     <h2>Працевлаштування</h2>
     <ul>
-        <li><a href="#" target="_blank">Фламандська служба зайнятості – VDAB</a></li>
-        <li><a href="#" target="_blank">Можливості працевлаштування – UNHCR Бельгія</a></li>
+        <li><a href="https://www.vdab.be" target="_blank">Фламандська служба зайнятості – VDAB</a></li>
+        <li><a href="https://www.jobinbelgium.be" target="_blank">Можливості працевлаштування – UNHCR Бельгія</a></li>
     </ul>
 
     <h2>Психологічна підтримка</h2>
     <ul>
-        <li><a href="#" target="_blank">Ментальне здоров'я – Mental Health Belgium</a></li>
-        <li><a href="#" target="_blank">Консультації – Карітас Бельгія</a></li>
+        <li><a href="https://www.mentalhealth.be" target="_blank">Ментальне здоров'я – Mental Health Belgium</a></li>
+        <li><a href="https://www.projects.be" target="_blank">Консультації – Карітас Бельгія</a></li>
     </ul>
 
     <h3>Контакти:</h3>
