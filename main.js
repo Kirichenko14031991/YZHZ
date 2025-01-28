8fef5a1905ad79dc1f01b3cf326c4d4a7e52755a
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2025-01-28 21:14:57 +0200

    ukr

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // main.js
 
-import { openModal, closeModal, closeOnOutsideClick, closeOnEsc } from "./src/components/modal.js";
+import {
+  openModal,
+  closeModal,
+  closeOnOutsideClick,
+  closeOnEsc,
+} from "./src/components/modal.js";
 import { initializeCityListClose } from "./src/components/cityList.js";
 import { openCityInfo } from "./src/components/cityInfo.js";
 import { openVidnInfo } from "./src/components/cities/austria/vidn.js";
@@ -29,26 +34,60 @@ initializeCityListClose();
 
 // Ініціалізація модального вікна
 const modalId = "registrationModal";
-document.getElementById("openModalBtn").onclick = () => openModal(modalId);
+document.getElementById("openModalBtn").onclick = () =>
+  openModal(modalId);
 document.querySelectorAll(".close").forEach((closeBtn) => {
   closeBtn.addEventListener("click", () => closeModal(modalId));
 });
 closeOnOutsideClick(modalId);
 closeOnEsc(modalId);
 
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .getElementById("registrationForm")
+    .addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const emailField = document.querySelector(
+        'input[name="user_email"]'
+      );
+      const emailValue = emailField.value;
+
+      if (!emailValue.includes("@")) {
+        alert("Будь ласка, додайте символ @ у вашу email-адресу!");
+        emailField.focus();
+        return;
+      }
+
+      alert("Ваші дані успішно відправлені!");
+    });
+});
+
 // Додавання обробників подій для міст
-document.getElementById("vidn").addEventListener("click", openVidnInfo);
-document.getElementById("graz").addEventListener("click", openGrazInfo);
+document
+  .getElementById("vidn")
+  .addEventListener("click", openVidnInfo);
+document
+  .getElementById("graz")
+  .addEventListener("click", openGrazInfo);
 document.getElementById("zal").addEventListener("click", openZalInfo);
 document.getElementById("lin").addEventListener("click", openLinInfo);
 document.getElementById("ins").addEventListener("click", openInsInfo);
 document.getElementById("bru").addEventListener("click", openBruInfo);
 document.getElementById("ant").addEventListener("click", openAntInfo);
-document.getElementById("gent").addEventListener("click", openGentInfo);
-document.getElementById("brug").addEventListener("click", openBrugInfo);
-document.getElementById("liez").addEventListener("click", openLiezInfo);
+document
+  .getElementById("gent")
+  .addEventListener("click", openGentInfo);
+document
+  .getElementById("brug")
+  .addEventListener("click", openBrugInfo);
+document
+  .getElementById("liez")
+  .addEventListener("click", openLiezInfo);
 document.getElementById("sof").addEventListener("click", openSofInfo);
-document.getElementById("plov").addEventListener("click", openPlovInfo);
+document
+  .getElementById("plov")
+  .addEventListener("click", openPlovInfo);
 document.getElementById("var").addEventListener("click", openVarInfo);
 document.getElementById("bur").addEventListener("click", openBurInfo);
 document.getElementById("rus").addEventListener("click", openRusInfo);
@@ -57,4 +96,3 @@ document.getElementById("spl").addEventListener("click", openSplInfo);
 document.getElementById("rie").addEventListener("click", openRieInfo);
 document.getElementById("dub").addEventListener("click", openDubInfo);
 document.getElementById("osi").addEventListener("click", openOsiInfo);
-
