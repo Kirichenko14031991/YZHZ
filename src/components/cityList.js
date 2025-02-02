f79678233b2e7c431d47af5d4ae96ded9ec7bb27
Kirichenko14031991 <124730976+Kirichenko14031991@users.noreply.github.com>
2025-02-02 18:25:32 +0200

    Update cityList.js

--- src/components/cityList.js
+++ src/components/cityList.js
@@ -1,32 +1,34 @@
 // cityList.js
+// cityList.js
 export function closeCityList() {
-    document.getElementById("cityList").style.display = "none";
-  }
-  
-  export function openCityList() {
-    document.getElementById("cityList").style.display = "block";
-  }
-  
-  export function initializeCityListClose() {
-    document.querySelector(".close-city-list").addEventListener("click", () => {
+  document.getElementById("cityList").style.display = "none";
+}
+
+export function openCityList() {
+  document.getElementById("cityList").style.display = "block";
+}
+
+export function initializeCityListClose() {
+  document
+    .querySelector(".close-city-list")
+    .addEventListener("click", () => {
       closeCityList();
     });
-  
-    window.addEventListener("click", (event) => {
-      const cityList = document.getElementById("cityList");
-      if (
-        event.target !== cityList &&
-        !cityList.contains(event.target) &&
-        event.target.tagName !== "A"
-      ) {
-        closeCityList();
-      }
-    });
-  
-    document.addEventListener("keydown", (event) => {
-      if (event.key === "Escape") {
-        closeCityList();
-      }
-    });
-  }
-  
+
+  window.addEventListener("click", (event) => {
+    const cityList = document.getElementById("cityList");
+    if (
+      event.target !== cityList &&
+      !cityList.contains(event.target) &&
+      event.target.tagName !== "A"
+    ) {
+      closeCityList();
+    }
+  });
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      closeCityList();
+    }
+  });
+}
